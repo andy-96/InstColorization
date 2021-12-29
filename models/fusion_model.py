3e55111c156c2af387f086da409f85cd1c73a640
Andy Chen <47676621+andy-96@users.noreply.github.com>
2021-12-29 18:26:19 +0100

    Update fusion_model.py

--- models/fusion_model.py
+++ models/fusion_model.py
@@ -94,7 +94,6 @@ class FusionModel(BaseModel):
     def setup_to_test(self, fusion_weight_path):
         root_path = os.path.join(os.path.split(__file__)[0], '..')
         GF_path = os.path.join(root_path, 'checkpoints/{0}/latest_net_GF.pth'.format(fusion_weight_path))
-        print(os.path.exists(GF_path))
         print('load Fusion model from %s' % GF_path)
         GF_state_dict = torch.load(GF_path)
         
